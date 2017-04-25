40572aa8a39fba4348c52a0bdcc3f9976c67f63b
Joakim Waltersson <joakim.waltersson@gmail.com>
2017-04-25 21:20:25 +0200

    Switch to request-promise-native to allow easier data aggregation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var request = require('request');
+var rp = require('request-promise-native');
 var cheerio = require('cheerio');
 var detailsFromHtml = require('./diva');
 
@@ -6,29 +6,25 @@ const baseUrl = 'http://liu.diva-portal.org';
 const noOfRows = 2;
 const url = baseUrl + '/smash/resultList.jsf?dswid=-6418&language=en&searchType=RESEARCH&query=&af=%5B%5D&aq=%5B%5B%5D%5D&aq2=%5B%5B%7B%22dateIssued%22%3A%7B%22from%22%3A%222016%22%2C%22to%22%3A%222016%22%7D%7D%2C%7B%22organisationId%22%3A%223801%22%2C%22organisationId-Xtra%22%3Atrue%7D%2C%7B%22publicationTypeCode%22%3A%5B%22article%22%5D%7D%5D%5D&aqe=%5B%5D&noOfRows=' + noOfRows + '&sortOrder=author_sort_asc&onlyFullText=false&sf=all';
 
-request(url, (error, response, html) => {
-    if (!error) {
-        console.log('Got OK response')
-        var $ = cheerio.load(html);
-        var publications = $('#formSmash\\:items\\:resultList_list > li');
+rp(url).then(html => {
+    console.log('Got OK response')
+    var $ = cheerio.load(html);
+    var publications = $('#formSmash\\:items\\:resultList_list > li');
 
-        publications.each(function (i, elem) {
-            //console.log($(elem).find('a').first().text());
-            //console.log($(elem).find('a').first().attr('href'));
-            const info = articleInfo($(elem).find('a').first().attr('href'));
-        });
-    } else {
-        console.log(error);
-    }
+    publications.each(function (i, elem) {
+        //console.log($(elem).find('a').first().text());
+        //console.log($(elem).find('a').first().attr('href'));
+        const info = articleInfo($(elem).find('a').first().attr('href'));
+    });
+}).catch(err => {
+    console.log(error);
 });
 
 function articleInfo(url) {
-    request(baseUrl + url, (error, response, html) => {
-        if (!error) {
-            return detailsFromHtml(html);
-        } else {
-            console.log(error);
-        }
+    rp(baseUrl + url).then(html => {
+        return detailsFromHtml(html);
+    }).catch(error => {
+        console.log(error);
     });
 }
 
