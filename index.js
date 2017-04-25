525bb0900b9c1f67695cd54655c5f509c57e57bf
Joakim Waltersson <joakim.waltersson@gmail.com>
2017-04-25 21:42:24 +0200

    Refactor to use async/await

--- index.js
+++ index.js
@@ -6,26 +6,25 @@ const baseUrl = 'http://liu.diva-portal.org';
 const noOfRows = 2;
 const url = baseUrl + '/smash/resultList.jsf?dswid=-6418&language=en&searchType=RESEARCH&query=&af=%5B%5D&aq=%5B%5B%5D%5D&aq2=%5B%5B%7B%22dateIssued%22%3A%7B%22from%22%3A%222016%22%2C%22to%22%3A%222016%22%7D%7D%2C%7B%22organisationId%22%3A%223801%22%2C%22organisationId-Xtra%22%3Atrue%7D%2C%7B%22publicationTypeCode%22%3A%5B%22article%22%5D%7D%5D%5D&aqe=%5B%5D&noOfRows=' + noOfRows + '&sortOrder=author_sort_asc&onlyFullText=false&sf=all';
 
-rp(url).then(html => {
+async function cheerioScrape() {
+    const html = await rp(url);
     console.log('Got OK response')
     var $ = cheerio.load(html);
     var publications = $('#formSmash\\:items\\:resultList_list > li');
 
-    publications.each(function (i, elem) {
+    for (let elem of publications.toArray()) {
         //console.log($(elem).find('a').first().text());
         //console.log($(elem).find('a').first().attr('href'));
-        const info = articleInfo($(elem).find('a').first().attr('href'));
-    });
-}).catch(err => {
-    console.log(error);
-});
+        const info = await articleInfo($(elem).find('a').first().attr('href'));
+        console.log(info);
+    };
+}
 
-function articleInfo(url) {
-    rp(baseUrl + url).then(html => {
-        return detailsFromHtml(html);
-    }).catch(error => {
-        console.log(error);
-    });
+cheerioScrape();
+
+async function articleInfo(url) {
+    const html = await rp(baseUrl + url);
+    return detailsFromHtml(html);
 }
 
 async function scrape() {
